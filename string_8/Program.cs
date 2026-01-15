3e827173b92990e696e9ab0a875629ba93c06798
m1t0ri <dmitriyanpilogov25@gmail.com>
2026-01-16 00:57:50 +0200

    Strings 8

---
+++ string_8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace MyNamespace;  
+  
+class Program  
+{
+    static void Main(string[] args)
+    {
+        string input = "";
+
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+        
+        
+        
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Input a word");
+            input = Console.ReadLine();
+        }
+        
+        string textToLower = input.ToLower();
+        string result = "";
+
+        foreach (var c in textToLower)
+        {
+            if (vowels.Contains(c))
+            {
+                result += c;
+            }
+        }
+        Console.WriteLine("All vowels leters:");
+        Console.WriteLine(result);
+        
+        Console.WriteLine("Your word was:");
+        Console.WriteLine(input);
+        
+        
+        Console.ReadKey();
+
+    }  
+}
