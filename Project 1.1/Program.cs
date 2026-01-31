0379a21580d7393bede3adb404513b30c093268a
m1t0ri <dmitriyanpilogov25@gmail.com>
2026-01-31 20:48:26 +0200

    Magic stne

---
+++ Project 1.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;  
+  
+namespace MyNamespace;  
+  
+class Program  
+{
+    static void Main(string[] args)
+    {
+        string question = null;
+        do
+        {
+            Console.WriteLine("Write your question, and magic amethyst will answer(or write 143 to exit):");
+            question = Console.ReadLine();
+            
+            if (question == "143")
+            {
+                break;
+            }
+
+            Answer();
+            
+            Console.ReadKey();
+            
+            Console.Clear();
+
+
+        } while (true);
+        
+    }
+    static void Answer()
+    {
+        string answer = null;
+        int rnd = new Random().Next(1,4);
+        switch (rnd)
+        {
+            case 1:
+                answer = "Yes";
+                break;
+            case 2: 
+                answer = "No";
+                break;
+            case 3:
+                answer = "Maybe";
+                break;
+                
+        }
+        Console.WriteLine(answer);
+    }
+}
